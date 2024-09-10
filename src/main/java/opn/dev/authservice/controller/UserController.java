77600995b13e0ce87084459ea683e815ebbb7491
skillsworld46 <skillsworld46@gmail.com>
2024-09-10 20:37:49 +0800

    removed useless APIs from user controller

--- src/main/java/opn/dev/authservice/controller/UserController.java
+++ src/main/java/opn/dev/authservice/controller/UserController.java
@@ -1,12 +1,11 @@
 package opn.dev.authservice.controller;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import opn.dev.authservice.dao.User;
+import opn.dev.authservice.entity.User;
 import opn.dev.authservice.service.imp.UserServiceImp;
 
 @RestController
@@ -19,44 +18,10 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PostMapping("/register")
-    public ResponseEntity<User> registerUser(@RequestBody UserRegistrationRequest request) {
-        User newUser = userService.registerUser(
-                request.getUsername(),
-                request.getEmail(),
-                request.getRawPassword(),
-                "system" // assuming creation by system for registration
-        );
-        return ResponseEntity.ok(newUser);
+    @GetMapping("/{username}")
+    public User getMethodName(@PathVariable String username) {
+        return userService.findByUsername(username);
     }
+
 }
 
-class UserRegistrationRequest {
-    private String username;
-    private String email;
-    private String rawPassword;
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getRawPassword() {
-        return rawPassword;
-    }
-
-    public void setRawPassword(String rawPassword) {
-        this.rawPassword = rawPassword;
-    }
-}
